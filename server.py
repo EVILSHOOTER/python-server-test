c6cdccb1e1e11d55dd331601c56530ebdc2113e7
EVILSHOOTER <72513936+EVILSHOOTER@users.noreply.github.com>
2020-11-26 03:03:50 +0000

    Create server.py

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket, time, pickle
+
+HEADERSIZE = 10
+
+SERVER = socket.gethostname()
+
+# object with IPv4 and TCP/IP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bound to tuple (IP and port) - your server
+s.bind((SERVER, 1337)) # requires a port-forwarded computer
+# prepare queue for incoming connections
+s.listen(5)
+
+while True:
+    # allow anybody connecting
+    clientsocket, address = s.accept()
+    print(f"Connected with {address}")
+
+    #msg = "welcom 2 server!"
+    d = {1: "testing", 2: "message"}
+    msg = pickle.dumps(d)
+    print(msg)
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
+    clientsocket.send(msg)
+
+
+    '''
+    # test send with time
+    while True:
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}" + msg
+        clientsocket.send(bytes(msg, "utf-8"))
+    '''
+    # send info to client
+    #clientsocket.send(bytes("zis is da massage", "utf-8"))
+    #clientsocket.close()
